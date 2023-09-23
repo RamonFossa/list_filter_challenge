2a4a0bd08d22ede1efbe30742802c6a1ab275dcc
RamonFossa <ramon.fossa@gmail.com>
2023-09-23 15:46:43 -0300

    feat: adding test to not string numbers

--- test/list_filter_challenge_test.exs
+++ test/list_filter_challenge_test.exs
@@ -9,6 +9,7 @@ defmodule ListFilterChallengeTest do
       list4 = ["melon", "apple", "kiwi"]
       list5 = []
       list6 = ["1", "3", "11", "43", "orange", "6", "9", "99"]
+      list7 = [1, 2, 8, 9, 0, "5", "oiii"]
 
       assert ListFilterChallenge.count_odd_number(list1) == 3
       assert ListFilterChallenge.count_odd_number(list2) == 0
@@ -16,6 +17,7 @@ defmodule ListFilterChallengeTest do
       assert ListFilterChallenge.count_odd_number(list4) == 0
       assert ListFilterChallenge.count_odd_number(list5) == 0
       assert ListFilterChallenge.count_odd_number(list6) == 6
+      assert ListFilterChallenge.count_odd_number(list7) == 3
     end
   end
 end
